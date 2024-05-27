13444cecb2e2f284426df26951a630c9f9ada5db
The Notorious M.U.L.U <haylomolo@gmail.com>
2024-05-27 20:46:41 +0300

    :hammer: added Section component

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,27 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { EntryLogo } from './components';
+import { Section } from './components/Section/Section';
+import { I_BELIEVE, I_BELIEVE_PER_1, I_BELIEVE_PER_2, I_BELIEVE_PER_3, NIETZSCHE_QUOTE, WHY_WE_DO_THIS, WHY_WE_DO_THIS_PER } from './consts';
+import './app.less';
 
 export function App() {
   return (
-    <EntryLogo duration={3000} children={<div>main page should be here</div>} />
+    <EntryLogo
+      duration={3000}
+      children={
+        <div className="app-container">
+          <h1>LAKSHMI</h1>
+          <Section
+            headline={NIETZSCHE_QUOTE}
+            paragraphs={[WHY_WE_DO_THIS_PER]}
+          />
+          <Section
+            headline={I_BELIEVE}
+            paragraphs={[I_BELIEVE_PER_1, I_BELIEVE_PER_2, I_BELIEVE_PER_3]}
+          />
+        </div>
+      }
+    />
   );
 }
 
