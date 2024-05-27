048e4de4ec0705b408be8163441ab96341153046
The Notorious M.U.L.U <haylomolo@gmail.com>
2024-05-27 20:46:41 +0300

    :hammer: added Section component

---
+++ src/app/components/Section/Section.tsx
@@ -0,0 +1,16 @@
+import './Section.less';
+export type SectionType = {
+  headline: string;
+  paragraphs: string[];
+};
+
+export const Section = ({ headline, paragraphs }: SectionType) => {
+  return (
+    <div className="section-container">
+      <h2 className="headline">{headline}</h2>
+      {paragraphs.map((paragraph) => (
+        <p className="paragraph">{paragraph}</p>
+      ))}
+    </div>
+  );
+};
